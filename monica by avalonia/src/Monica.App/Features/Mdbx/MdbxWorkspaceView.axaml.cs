076df210940b1c4e37e2b6bdecac5e4d2fb43209
JoyinJoester <lichaoran8@gmail.com>
2026-07-13 16:12:52 +0800

    refactor: extract mdbx workspace

---
+++ monica by avalonia/src/Monica.App/Features/Mdbx/MdbxWorkspaceView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Monica.App.Features.Mdbx;
+
+public partial class MdbxWorkspaceView : UserControl
+{
+    public MdbxWorkspaceView()
+    {
+        InitializeComponent();
+    }
+}
