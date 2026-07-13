9693331c3029a84cac1f78759f0bca7a635975e4
JoyinJoester <lichaoran8@gmail.com>
2026-07-13 13:32:56 +0800

    refactor: extract authenticator workspace view

---
+++ monica by avalonia/src/Monica.App/Features/Authenticator/AuthenticatorWorkspaceView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Monica.App.Features.Authenticator;
+
+public partial class AuthenticatorWorkspaceView : UserControl
+{
+    public AuthenticatorWorkspaceView()
+    {
+        InitializeComponent();
+    }
+}
