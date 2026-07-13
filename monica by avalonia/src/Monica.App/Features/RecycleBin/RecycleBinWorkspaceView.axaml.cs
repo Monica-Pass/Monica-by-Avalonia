0d700454ce61c17e0f0d6eda165f5f1bdcb99e1a
JoyinJoester <lichaoran8@gmail.com>
2026-07-13 14:24:34 +0800

    refactor: extract recycle bin workspace

---
+++ monica by avalonia/src/Monica.App/Features/RecycleBin/RecycleBinWorkspaceView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Monica.App.Features.RecycleBin;
+
+public partial class RecycleBinWorkspaceView : UserControl
+{
+    public RecycleBinWorkspaceView()
+    {
+        InitializeComponent();
+    }
+}
