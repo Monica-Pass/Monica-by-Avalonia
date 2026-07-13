f9746751fd738669526783ea7726337a033386dd
JoyinJoester <lichaoran8@gmail.com>
2026-07-13 15:53:42 +0800

    refactor: extract sync workspace

---
+++ monica by avalonia/src/Monica.App/Features/Sync/SyncWorkspaceView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Monica.App.Features.Sync;
+
+public partial class SyncWorkspaceView : UserControl
+{
+    public SyncWorkspaceView()
+    {
+        InitializeComponent();
+    }
+}
