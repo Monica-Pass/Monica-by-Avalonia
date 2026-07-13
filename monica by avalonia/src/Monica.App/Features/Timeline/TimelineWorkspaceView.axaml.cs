70dae754a8b37e523d34c04e25a612c14bcacf2d
JoyinJoester <lichaoran8@gmail.com>
2026-07-13 16:22:04 +0800

    refactor: extract timeline workspace

---
+++ monica by avalonia/src/Monica.App/Features/Timeline/TimelineWorkspaceView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Monica.App.Features.Timeline;
+
+public partial class TimelineWorkspaceView : UserControl
+{
+    public TimelineWorkspaceView()
+    {
+        InitializeComponent();
+    }
+}
