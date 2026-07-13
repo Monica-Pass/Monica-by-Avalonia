f0ecb81e154e0881af69114b3949625ba90eed38
JoyinJoester <lichaoran8@gmail.com>
2026-07-13 13:59:13 +0800

    refactor: extract generator workspace

---
+++ monica by avalonia/src/Monica.App/Features/Generator/GeneratorWorkspaceView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Monica.App.Features.Generator;
+
+public partial class GeneratorWorkspaceView : UserControl
+{
+    public GeneratorWorkspaceView()
+    {
+        InitializeComponent();
+    }
+}
