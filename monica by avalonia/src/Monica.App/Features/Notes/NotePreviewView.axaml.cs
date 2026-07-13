789898dc282df81653dc220b37b3d00ba766d7ec
JoyinJoester <lichaoran8@gmail.com>
2026-07-13 12:36:54 +0800

    refactor: extract secure note preview view

---
+++ monica by avalonia/src/Monica.App/Features/Notes/NotePreviewView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Monica.App.Features.Notes;
+
+public partial class NotePreviewView : UserControl
+{
+    public NotePreviewView()
+    {
+        InitializeComponent();
+    }
+}
