d89b621747032fe759d65448299be50f508f0278
JoyinJoester <lichaoran8@gmail.com>
2026-07-13 17:23:32 +0800

    refactor: extract security analysis workspace

---
+++ monica by avalonia/src/Monica.App/Features/SecurityAnalysis/SecurityAnalysisWorkspaceView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Monica.App.Features.SecurityAnalysis;
+
+public partial class SecurityAnalysisWorkspaceView : UserControl
+{
+    public SecurityAnalysisWorkspaceView()
+    {
+        InitializeComponent();
+    }
+}
