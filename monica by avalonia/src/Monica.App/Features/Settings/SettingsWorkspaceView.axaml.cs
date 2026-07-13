aa8c3f3907fcea7abff4ae79634e153161c8d33e
JoyinJoester <lichaoran8@gmail.com>
2026-07-13 14:46:52 +0800

    refactor: extract settings workspace

---
+++ monica by avalonia/src/Monica.App/Features/Settings/SettingsWorkspaceView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Monica.App.Features.Settings;
+
+public partial class SettingsWorkspaceView : UserControl
+{
+    public SettingsWorkspaceView()
+    {
+        InitializeComponent();
+    }
+}
